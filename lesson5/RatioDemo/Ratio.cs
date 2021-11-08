d62493cc0d7f4df6553c32c96e3fd99daa46acd8
Сергей <saygon707@gmail.com>
2021-11-09 00:07:02 +0300

    основа класса

--- lesson5/RatioDemo/Ratio.cs
+++ lesson5/RatioDemo/Ratio.cs
@@ -8,5 +8,16 @@ namespace RatioDemo
 {
     class Ratio
     {
+        private int numerator; //числитель
+        private int denominator; //знаменатель
+
+        public int Numerator { get { return numerator; } }
+        public int Denominator { get { return denominator; } }
+
+        public Ratio(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
     }
 }
