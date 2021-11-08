ae0c976de84eb1174874f68ee0f6ce0e73454ac2
Сергей <saygon707@gmail.com>
2021-11-09 02:44:50 +0300

    добавил комплексные числа

---
+++ lesson5/RatioDemo/ComplexusDecimal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexusNum
+{
+    class ComplexusDecimal
+    {
+        private decimal a;
+        private decimal b;
+
+        public decimal A
+        {
+            get { return a; }
+        }
+        public decimal B
+        {
+            get { return b; }
+        }
+
+    }
+}
