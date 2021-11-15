41c51cd83241d2bc1ee4ef13909ae0ea46d653f8
Сергей <saygon707@gmail.com>
2021-11-15 04:40:10 +0300

    задание 3

---
+++ lesson6/Geometric/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometric
+{
+    class Point: Figure
+    {
+        public Point(string color, bool visible) : base(color, visible, 0, 0)
+        {
+
+        }
+        public Point(string color, bool visible, int positionX, int positionY): base(color, visible, positionX, positionY)
+        {
+
+        }
+    }
+}
